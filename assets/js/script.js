7f022a3eaa6e08abb3e5da1190eb9c0d48b318e0
Johnson ojo <johnsonojo2@gmail.com>
2022-05-22 19:22:40 -0600

    building the navigation menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
+//MODAL
 const modal = document.querySelector("[data-modal]");
 const modalCloseBtn = document.querySelector("[data-modal-close]");
 const modalCloseOverlay = document.querySelector("[data-modal-overlay]");
 
 //modal close function
-const modalCloseFunc = () => {
+const closeFunc = () => {
   modal.classList.add("closed");
 };
 
 //modal overlay close eventlistener
-modalCloseOverlay.addEventListener("click", modalCloseFunc);
+modalCloseOverlay.addEventListener("click", closeFunc);
 //modal  close button  eventlistener
-modalCloseBtn.addEventListener("click", modalCloseFunc);
+modalCloseBtn.addEventListener("click", closeFunc);
+
+//NOTIFICATION-TOAST
+const notificationToast = document.querySelector("[data-toast");
+const toastCloseBtn = document.querySelector("[data-toast-close");
+
+//notification toast eventListener
+toastCloseBtn.addEventListener("click", () => {
+  notificationToast.classList.add("closed");
+});
