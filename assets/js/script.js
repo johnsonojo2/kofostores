ef08305e9f6545b5097646ae962b0ba99e6f5860
Johnson ojo <johnsonojo2@gmail.com>
2022-05-28 16:43:16 -0600

    adding menu open and close functionalities to the mobile menu buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,30 @@ const toastCloseBtn = document.querySelector("[data-toast-close");
 toastCloseBtn.addEventListener("click", () => {
   notificationToast.classList.add("closed");
 });
+
+//MOBILE MENU
+
+const mobileMenuOpenBtn = document.querySelectorAll(
+  "[data-mobile-menu-open-btn]"
+);
+const mobileMenu = document.querySelectorAll("[data-mobile-menu]");
+const mobileMenuCloseBtn = document.querySelectorAll(
+  "[data-mobile-menu-close-btn]"
+);
+const overlay = document.querySelector("[data-overlay]");
+
+for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
+  // mobile menu open & close function
+  const mobileMenuCloseFunc = function () {
+    mobileMenu[i].classList.remove("active");
+    overlay.classList.remove("active");
+  };
+
+  mobileMenuOpenBtn[i].addEventListener("click", function () {
+    mobileMenu[i].classList.add("active");
+    overlay.classList.add("active");
+  });
+
+  mobileMenuCloseBtn[i].addEventListener("click", mobileMenuCloseFunc);
+  overlay.addEventListener("click", mobileMenuCloseFunc);
+}
